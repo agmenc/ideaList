f27af41c9ce13f3347c892f1e50acd8106cd639e
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-02-28 18:29:17 +0000

    Final ideaList test working

--- test/IdeaListSpec.js
+++ test/IdeaListSpec.js
@@ -26,35 +26,36 @@ describe('IdeaList', function () {
     });
 
     it('Allows the user to select a node', function () {
-        var firstItem = divToPopulate.find("li").first();
+        firstItem().click();
 
-        firstItem.click();
-
-        expect(firstItem.find("span").hasClass("selected")).toEqual(true);
+        expect(firstItem().find("span").hasClass("selected")).toEqual(true);
     });
     it('Displays options to add or delete child nodes', function () {
-        var firstItem = divToPopulate.find("li").first();
-
-        firstItem.click();
+        firstItem().click();
 
         expect(divToPopulate.html()).toContain('<div id="options" class="options"><a id="addChild" href="">add</a> | <a id="deleteChild" href="">delete</a></div>');
     });
     it('Adds a <ul></ul> containing a <li></li> when adding the first child node', function () {
-        var firstItem = divToPopulate.find("li").first();
-        firstItem.click();
+        firstItem().click();
 
-        var addButton = firstItem.find("a").first();
-        addButton.click();
+        addButton().click();
 
-        expect(firstItem.html()).toContain('<ul><li class="expanded"><span>Start Typing</span></li></ul>');
+        expect(firstItem().html()).toContain('<ul><li class="expanded"><span>Start Typing</span></li></ul>');
     });
     it('Only adds a <li></li> when adding subsequent child nodes', function () {
-        var firstItem = divToPopulate.find("li").first();
-        firstItem.click();
+        firstItem().click();
 
-        var addButton = firstItem.find("input").first();
-        addButton.click();
+        addButton().click();
+        addButton().click();
 
-        expect(firstItem.html()).toContain('<ul><li class="expanded"><span>Start Typing</span></li></ul>');
+        expect(firstItem().html()).toContain('<ul><li class="expanded"><span>Start Typing</span></li><li class="expanded"><span>Start Typing</span></li></ul>');
     });
+
+    function firstItem() {
+        return divToPopulate.find("li").first();
+    }
+
+    function addButton() {
+        return $("#options").find("a").first();
+    }
 });
