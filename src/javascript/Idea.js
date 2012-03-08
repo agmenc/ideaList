fe4802fb3922251e806d61dd694e962c03fb8192
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-03-08 19:32:40 +0000

    Taught Idea to inflate itself from a raw JSON string

--- src/javascript/Idea.js
+++ src/javascript/Idea.js
@@ -4,12 +4,24 @@ function Idea(description, children) {
     var desc = description;
     var kids = children ? children : [];
 
-    this.description = function() { return desc; };
-    this.children = function() { return kids; };
-    this.hasChildren = function() { return kids.length > 0; };
-    this.print = function() { return desc + " [" + kids.length + "]"; };
-    this.addChild = function(drugs) { kids.push(drugs); };
-    this.toJSON = function() {
-        return {description: desc, children: kids};
+    this.description = function () { return desc; };
+    this.children = function () { return kids; };
+    this.hasChildren = function () { return kids.length > 0; };
+    this.print = function () { return desc + " [" + kids.length + "]"; };
+    this.addChild = function (drugs) { kids.push(drugs); };
+    this.toJSON = function () {
+        return {description:desc, children:kids};
     };
 }
+
+Idea.inflateFrom = function (jsonString) {
+    return JSON.parse(jsonString, reviver);
+
+    function reviver(key, value) {
+        return validIdea(value) ? new Idea(value.description, value.children) : value;
+    }
+
+    function validIdea(value) {
+        return value && typeof value === "object" && value.description && value.children;
+    }
+};
--- test/IdeaSpec.js
+++ test/IdeaSpec.js
@@ -1,14 +1,22 @@
 //
 
+var ideaListAsString;
+var ideaListAsObject;
+
+beforeEach(function () {
+    ideaListAsString = '{"description":"Root node","children":[{"description":"Child","children":[{"description":"Grandchild","children":[]}]}]}';
+    ideaListAsObject = new Idea("Root node", [new Idea("Child", [new Idea("Grandchild")])]);
+});
+
 describe('Idea', function () {
     it('Knows how to Stringify itself to a strict JSON format', function () {
-        var idea = new Idea("Root node", [new Idea("Child", [new Idea("Grandchild")])]);
+        var stringified = JSON.stringify(ideaListAsObject);
 
-        var json = JSON.stringify(idea);
-
-        expect(json).toEqual('{"description":"Root node","children":[{"description":"Child","children":[{"description":"Grandchild","children":[]}]}]}');
+        expect(stringified).toEqual(ideaListAsString);
     });
     it('Knows how to inflate itself from a strict JSON format', function () {
-        expect(false).toEqual(true);
+        var inflated = Idea.inflateFrom(ideaListAsString);
+
+        expect(inflated).toEqual(ideaListAsObject);
     });
 });
